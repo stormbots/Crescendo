d770f5289f7fdc9ce107c62f14b5d504f4aed7e7
Michael Wilde <techbub619@gmail.com>
2024-01-18 18:19:08 -0800

    Basic setup for intake and passthrough (WP), no controls set or CANID

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,11 +4,36 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
+  //Define SparkMax
+  public CANSparkMax motor = new CANSparkMax(30, MotorType.kBrushless);
+  //Define motor speed, adjust
+  double kIntakeSpeed = 0.1;
+
   /** Creates a new Intake. */
-  public Intake() {}
+  public Intake() {
+    //Safety
+    motor.setSmartCurrentLimit(30);
+  }
+
+
+  //Intake On
+  public void intakeOn(){
+    motor.set(kIntakeSpeed);
+  }
+  //Intake Off
+  public void intakeOff(){
+    motor.set(0.0);
+  }
+  //Intake Eject (use when object stuck)
+  public void intakeEject(){
+    motor.set(-kIntakeSpeed);
+  }
 
   @Override
   public void periodic() {
--- src/main/java/frc/robot/subsystems/Passthrough.java
+++ src/main/java/frc/robot/subsystems/Passthrough.java
@@ -4,11 +4,46 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Passthrough extends SubsystemBase {
+  //Define SparkMax
+  public CANSparkMax motor = new CANSparkMax(30, MotorType.kBrushless);;
+  //Define motor speed, adjust
+  double kPassthroughSpeed = 0.1;
+  //Additional Sensor (unknown currently ;( )
+
+
   /** Creates a new Passthrough. */
-  public Passthrough() {}
+  public Passthrough() {
+    //Safety inplace
+    motor.setSmartCurrentLimit(30);
+  }
+
+  //This for manual option for driver
+  //Passthrough On
+  public void passthoughOn(){
+    motor.set(kPassthroughSpeed);
+  }
+  //Passthrough Off
+  public void passthoughOff() {
+    motor.set(0.0);
+  }
+  //Passthrough Out
+  public void passthroughOut() {
+    motor.set(-kPassthroughSpeed);
+  }
+  //This will be area for setup for sensor
+  public void sensorStop() {
+    //if someSensor == no note/orange not dected
+    // passthroughOn
+    //if someSensor == note/orange dected
+    // passthroughOff
+     
+  }
 
   @Override
   public void periodic() {
