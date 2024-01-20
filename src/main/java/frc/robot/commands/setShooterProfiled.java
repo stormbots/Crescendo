9e9eba4a76dffd87c8b655f46552075a523cf443
kimmpy <kim.kp.pham@gmail.com>
2024-01-20 13:54:00 -0800

    shooter

---
+++ src/main/java/frc/robot/commands/setShooterProfiled.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Shooter;
+
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class setShooterProfiled extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Shooter shooter;
+  private double shooterAngle;
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(0, 0);
+  TrapezoidProfile.State goal = new TrapezoidProfile.State(0, 0);
+  TrapezoidProfile.State initial = new TrapezoidProfile.State(0, 0);
+  TrapezoidProfile shooterProfile = new TrapezoidProfile(constraints);
+  double startTimer = 0;
+
+  public setShooterProfiled(double shooterAngle, Shooter shooter) {
+    this.shooterAngle = shooterAngle;
+    this.shooter = shooter;
+    addRequirements(shooter);
+    goal = new TrapezoidProfile.State(shooterAngle, 0);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    startTimer = Timer.getFPGATimestamp();
+    initial = new TrapezoidProfile.State(shooter.getShooterAngle(), shooter.shooterMotor.getEncoder().getVelocity());
+    shooterProfile = new TrapezoidProfile(constraints);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    var targetPosition = shooterProfile.calculate(Timer.getFPGATimestamp()-startTimer, initial, goal).position;
+    shooter.setShooterPID(targetPosition);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shooter.shooterMotor.set(0.0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,14 +4,77 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Lerp;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
-  public Shooter() {}
+  public CANSparkMax shooterMotor = new CANSparkMax(21, MotorType.kBrushless);
+  private SparkPIDController pidController = shooterMotor.getPIDController();
+  private DutyCycleEncoder shooterAbsEncoder = new DutyCycleEncoder(21);
+  private double shooterSetPoint = 0.0;
+  private Lerp shooterAnalogLerp = new Lerp(0, 0, 0, 0);
+
+  //temp values
+  private IdleMode idle;
+  private CANSparkMax.ControlType controlType;
+
+
+  public Shooter() {
+    shooterMotor.restoreFactoryDefaults();
+
+    pidController = shooterMotor.getPIDController();
+
+    //closed-loop control
+    double kP = 0;
+    double kI = 0;
+    double kD = 0;
+    double kIz = 0;
+    double kFF = 0;
+    double kMaxOutput = 0;
+    double kMinOutput = -0.3;
+
+    pidController.setP(kP);
+    pidController.setI(kI);
+    pidController.setD(kD);
+    pidController.setIZone(kIz);
+    pidController.setFF(kFF);
+    pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    //current limits?
+    //soft limits
+
+    shooterMotor.setIdleMode(idle);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void moveShooter(double speed) {
+    shooterMotor.set(speed);
+  }
+
+  public double getShooterAngle() {
+    return shooterMotor.getEncoder().getPosition();
+  }
+
+  public double getShooterAngleAbsolute() {
+    return shooterAbsEncoder.get(); //in rotations, need to do limit
+  }
+
+  public void setShooterPID(double setPoint) {
+    this.shooterSetPoint = setPoint;
+    var shooterFF = Lerp.lerp(0, 0, 0, 0, 0);
+    shooterFF*=Math.cos(Math.toRadians(getShooterAngle()));
+    pidController.setReference(setPoint, controlType, 21, shooterFF); //TODO: voltage control
   }
 }
