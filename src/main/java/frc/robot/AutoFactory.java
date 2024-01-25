cb7307077af9cdd5d312c3a6184e65c06521fe2e
bigbrainbrian7 <brianhyu08@gmail.com>
2024-01-24 18:56:32 -0800

    Finish up trajectory + pathplanner config

--- src/main/java/frc/robot/AutoFactory.java
+++ src/main/java/frc/robot/AutoFactory.java
@@ -4,19 +4,31 @@
 
 package frc.robot;
 
+import java.util.List;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import frc.robot.ChassisConstants.AutoConstants;
+import frc.robot.ChassisConstants.DriveConstants;
+import frc.robot.ChassisConstants.ModuleConstants;
 
 /** Add your docs here. */
 public class AutoFactory {
@@ -30,35 +42,50 @@ public class AutoFactory {
     public AutoFactory(RobotContainer rc){
         this.rc = rc;
 
-        //im too lazy to do this for now so i give up
+        trajectoryConfig.setKinematics(rc.swerveDriveKinematics);
+
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+        Supplier<ChassisSpeeds> getSpeed = ()->{
+            return rc.swerveDriveKinematics.toChassisSpeeds(rc.chassis.getWheelStates());
+        };
+
+        Consumer<ChassisSpeeds> drive = (chassisSpeeds)->{
+            //is field relative? + no idea how this works as max should be 1, will check later.
+            rc.chassis.drive(
+                chassisSpeeds.vxMetersPerSecond/AutoConstants.kMaxSpeedMetersPerSecond, 
+                chassisSpeeds.vxMetersPerSecond/AutoConstants.kMaxSpeedMetersPerSecond, 
+                chassisSpeeds.omegaRadiansPerSecond/AutoConstants.kMaxAngularSpeedRadiansPerSecond, 
+                false, false);
+        };
+
+        HolonomicPathFollowerConfig holonomicPathFollowerConfig = new HolonomicPathFollowerConfig(
+            new PIDConstants(ModuleConstants.kDrivingP), 
+            new PIDConstants(ModuleConstants.kTurningP), 
+            AutoConstants.kMaxSpeedMetersPerSecond, 
+            Math.sqrt(Math.pow(DriveConstants.kTrackWidth/2, 2) + Math.pow(DriveConstants.kWheelBase/2, 2)),
+            new ReplanningConfig(false, false));
+
+        BooleanSupplier shouldFlipPath = () -> {
+            // Boolean supplier that controls when the path will be mirrored for the red alliance
+            // This will flip the path being followed to the red side of the field.
+            // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+            var alliance = DriverStation.getAlliance();
+            if (alliance.isPresent()) {
+                return alliance.get() == DriverStation.Alliance.Red;
+            }
+            return false;
+        };
+
         AutoBuilder.configureHolonomic(
             rc.swerveDrivePoseEstimator::getEstimatedPosition, 
             rc.chassis::resetPose, 
-            rc.swerveDrivePoseEstimator, 
-            (chassisSpeeds)->{
-                    //is field relative? + no idea how this works as max should be 1, will check later.
-                    rc.chassis.drive(
-                        chassisSpeeds.vxMetersPerSecond/AutoConstants.kMaxSpeedMetersPerSecond, 
-                        chassisSpeeds.vxMetersPerSecond/AutoConstants.kMaxSpeedMetersPerSecond, 
-                        chassisSpeeds.omegaRadiansPerSecond/AutoConstants.kMaxAngularSpeedRadiansPerSecond, 
-                        true, false);}, 
-            null, 
-            () -> {
-                    // Boolean supplier that controls when the path will be mirrored for the red alliance
-                    // This will flip the path being followed to the red side of the field.
-                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-
-                    var alliance = DriverStation.getAlliance();
-                    if (alliance.isPresent()) {
-                        return alliance.get() == DriverStation.Alliance.Red;
-                    }
-                    return false;
-                }, 
+            getSpeed, 
+            drive,
+            holonomicPathFollowerConfig,
+            shouldFlipPath,  
             rc.chassis);
-
-            trajectoryConfig.setKinematics(rc.swerveDriveKinematics);
-
-            thetaController.enableContinuousInput(-Math.PI, Math.PI);
     }
 
     SwerveControllerCommand generateTrajectory(Trajectory trajectory){
@@ -89,6 +116,14 @@ public class AutoFactory {
 
     public Command ExampleAuto(){
         return new InstantCommand(()->{},rc.chassis,rc.climber);
+    }
+
+    public SwerveControllerCommand getSwerveControllerCommand(){
+        return generateTrajectory(TrajectoryGenerator.generateTrajectory(
+          new Pose2d(0, 0, new Rotation2d(0)),
+          List.of(),
+          new Pose2d(1,0, new Rotation2d(0)),
+          trajectoryConfig));
     }
 
 }
--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics.SwerveDriveWheelStates;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.util.WPIUtilJNI;
@@ -265,5 +266,14 @@ public class Chassis extends SubsystemBase {
       new SwerveModulePosition(pose.getX(), pose.getRotation()),
       new SwerveModulePosition(pose.getX(), pose.getRotation())
   }, pose);
+  }
+
+  public SwerveDriveWheelStates getWheelStates(){
+    return new SwerveDriveWheelStates(new SwerveModuleState[] {
+      frontLeft.getState(),
+      frontRight.getState(),
+      rearLeft.getState(),
+      rearRight.getState()
+  });
   }
 }
