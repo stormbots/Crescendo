98104954b839dababb75b527c829aead17e209cd
fortierkids1 <fortierkids1@gmail.com>
2024-02-05 20:44:39 -0800

    Working rgb to hsv

--- src/main/java/frc/robot/commands/LightingProgressBar.java
+++ src/main/java/frc/robot/commands/LightingProgressBar.java
@@ -3,28 +3,31 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.LEDs;
 
+  
 public class LightingProgressBar extends Command {
   /** Creates a new LightingColor. */
-  final LEDs leds;
+  LEDs leds;
   Color backGroundColor;
   Color progressColor;
   double timeLimt;
   double startTime;
   boolean finished;
+
+
   public LightingProgressBar(LEDs leds, Color backGroundColor, Color progressColor, double timeLimt) {
     this.leds = leds;
     this.backGroundColor = backGroundColor;
     this.progressColor = progressColor;
     this.timeLimt = timeLimt;
+
+    addRequirements(leds);
   }
     // Use addRequirements() here to declare subsystem dependencies.
+    
 
   // Called when the command is initially scheduled.
   @Override
@@ -32,18 +35,15 @@ public class LightingProgressBar extends Command {
     finished = false;
     // leds.ledStrip.setData(leds.ledBuffer);
     // leds.ledStrip.start();
-    startTime = Timer.getFPGATimestamp();
-    leds.setLedRGBLib(backGroundColor);
+    startTime = leds.getStartTime();
+    leds.setLedRGB(backGroundColor);
     
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute(){
-    SmartDashboard.putNumber("StartTime", startTime);
-    
-    var currentTime = Timer.getFPGATimestamp();
-    SmartDashboard.putNumber("CurrentTime", currentTime);
+    var currentTime = leds.getCurrentTime();
     var elapsedTime = currentTime-startTime;
     double timePerLED = (timeLimt / leds.ledBuffer.getLength());
     var channels = (int)(Math.round((elapsedTime / timePerLED)));
@@ -51,7 +51,7 @@ public class LightingProgressBar extends Command {
       finished = true;
       channels = leds.ledBuffer.getLength();
     }
-    leds.setLedRGBLib(progressColor, channels);
+    leds.setLedRGB(progressColor, channels);
     // if (channels + 1 > leds.ledBuffer.getLength()){
     //   finished = true;
     // }
@@ -63,7 +63,7 @@ public class LightingProgressBar extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    leds.setLedRGBLib(backGroundColor);
+    leds.setLedRGB(backGroundColor);
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -7,7 +7,9 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -18,7 +20,6 @@ public class LEDs extends SubsystemBase {
   public AddressableLED ledStrip;
   public AddressableLEDBuffer ledBuffer;
   private int rainbowStart;
-  double startTime;
   /** Creates a new LEDs. */
   public LEDs() {
     ledStrip = new AddressableLED(9);
@@ -26,52 +27,122 @@ public class LEDs extends SubsystemBase {
     ledStrip.setLength(ledBuffer.getLength());
     ledStrip.setData(ledBuffer);
     ledStrip.start();
-    startTime = 0;
     rainbowStart = 0;
   }
 
   @Override
   public void periodic() {
+    // setLedHsvManual(206/2, 156, 180);
     ledStrip.setData(ledBuffer);
     // This method will be called once per scheduler run
   }
 
-  public void setBuffer(AddressableLEDBuffer buffer){
-    ledStrip.setData(buffer);
-  }
-
+  
   public double getStartTime(){
-    startTime = Timer.getFPGATimestamp();
+    double startTime = Timer.getFPGATimestamp();
     return startTime;
   }
 
-  public void setLedHSV(int hue, int saturation, int value){
+  public double getCurrentTime(){
+    double currentTime = Timer.getFPGATimestamp();
+    return currentTime;
+  }
+
+ public int[] rgbToHsv(Color color){
+  double r = color.red;
+  double g = color.green;
+  double b = color.blue;
+  double max = Math.max(r, Math.max(g, b)); // maximum of r, g, b 
+  double min = Math.min(r, Math.min(g, b)); // minimum of r, g, b 
+  double range = max - min; // diff of cmax and cmin. 
+  double h = -1, s = -1; 
+  if (max == min) 
+    h = 0; 
+
+  // if cmax equal r then compute h 
+  else if (max == r) 
+    h = ((60 * ((g - b) / range) + 360) % 360)/2; 
+
+  // if cmax equal g then compute h 
+  else if (max == g) 
+    h = ((60 * ((b - r) / range) + 120) % 360)/2; 
+
+  // if cmax equal b then compute h 
+  else if (max == b) 
+    h = ((60 * ((r - g) / range) + 240) % 360)/2; 
+
+  // if cmax equal zero 
+  if (max == 0) 
+    s = 0; 
+  else
+    s = (range / max) * 255; 
+
+  // compute v 
+  double v = max * 255; 
+  int[] hsv = new int[3];
+  hsv[0] = (int)Math.round(h);
+  hsv[1] = (int)Math.round(s);
+  hsv[2] = (int)Math.round(v);
+  return hsv;
+ } 
+
+  private void setLedHsvManual(int hue, int saturation, int value){
     for(var i = 0; i < ledBuffer.getLength(); i++){
       ledBuffer.setHSV(i, hue, saturation, value);
     }
   }
 
-  public void setLedRGB(int red, int green, int blue){
+  private void setLedRgbManual(int red, int green, int blue){
     for(var i = 0; i < ledBuffer.getLength(); i++){
       ledBuffer.setRGB(i, red, green, blue);
     }
   }
 
-  public void setLedRGBLib(Color color){
+  public int matchBrightnessScaling(int disabledBrightness, int enabledBrightness){
+    if (DriverStation.isDisabled()){
+      return disabledBrightness;
+    }
+    return enabledBrightness;
+  }
+
+  public void setLedRGB(Color color){
     int red = (int)(color.red*255);
     int green = (int)(color.green*255);
     int blue = (int)(color.blue*255);
+    SmartDashboard.putNumber("led/Red", red);
+    SmartDashboard.putNumber("led/Green", green);
+    SmartDashboard.putNumber("led/Blue", blue);
     for(var i = 0; i < ledBuffer.getLength(); i++){
       ledBuffer.setRGB(i, red, green, blue);
     }
   }
 
-  public void setLedRGBLib(Color color, double channels){
+  public void setLedRGB(Color color, double channels){
     int red = (int)(color.red*255);
     int green = (int)(color.green*255);
     int blue = (int)(color.blue*255);
     for(var i = 0; i < channels; i++){
       ledBuffer.setRGB(i, red, green, blue);
+    }
+  }
+
+  public void setLedHSV(Color color, int percentOutput){
+    int[] hsv = rgbToHsv(color);
+    int hue = hsv[0];
+    int saturation = hsv[1];
+    int value = (int) Math.round((hsv[2]*percentOutput/100));
+    for(var i = 0; i < ledBuffer.getLength(); i++){
+      ledBuffer.setHSV(i, hue, saturation, value);
+    }
+  }
+
+  public void setLedHSV(Color color, int percentOutput, double channels){
+    int[] hsv = rgbToHsv(color);
+    int hue = hsv[0];
+    int saturation = hsv[1];
+    int value = (int) Math.round((hsv[2]*percentOutput/100));
+    for(var i = 0; i < ledBuffer.getLength(); i++){
+      ledBuffer.setHSV(i, hue, saturation, value);
     }
   }
 
@@ -89,24 +160,24 @@ public class LEDs extends SubsystemBase {
         var color = DriverStation.getAlliance();
         if (color.isPresent()){
           if (color.get() == DriverStation.Alliance.Red){
-            this.setLedRGBLib(Color.kRed);
+            this.setLedHSV(Color.kRed, this.matchBrightnessScaling(10, 100));
           }
           if (color.get() == DriverStation.Alliance.Blue){
-            this.setLedRGBLib(Color.kBlue);
+            this.setLedHSV(Color.kBlue, this.matchBrightnessScaling(10, 100));
           }
           return;
         }
-        this.setLedRGBLib(Color.kPurple);
+        this.setLedHSV(Color.kPurple, 10);
       },this)
       .ignoringDisable(true)
       ;
     }
 
     public Command showNoteIntake(){
-      return new RunCommand(()->this.setLedRGBLib(Color.kOrangeRed),this)
+      return new RunCommand(()->this.setLedRGB(Color.kOrangeRed),this)
       .withTimeout(2)
       ;
     }
-
+    
   }
 
