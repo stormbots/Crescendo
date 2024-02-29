e8e1fe7aa1b1eb7d97bee5fba8cb068ce9e61b23
Stormwork2 <tekdemo+driverstation@gmail.com>
2024-02-28 18:40:08 -0800

    Working red+blue side autos for one note shoot + intake (much cleaner!!!!)

--- src/main/java/frc/robot/AutoFactory.java
+++ src/main/java/frc/robot/AutoFactory.java
@@ -69,19 +69,34 @@ public class AutoFactory {
 
     public AutoFactory(RobotContainer rc){
         this.rc = rc;
-        // initTrajectoryStuff();
-        initPathPlannerStuff();
+        // initPathPlannerStuff();
 
         thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
         autoChooser.setDefaultOption("Please Select Auto", new InstantCommand());
-        autoChooser.addOption("blue center", makeBasicAuto(new Pose2d(1.2, 5.55, new Rotation2d(0)), new Pose2d(2.9, 5.55, new Rotation2d(0))));
-        autoChooser.addOption("blue source", makeBasicAuto(new Pose2d(0.8, 4.4, new Rotation2d(-60)),List.of(new Translation2d(0.8, 4.2)), new Pose2d(2.9, 4.1, new Rotation2d(0))));
-        autoChooser.addOption("blue amp", makeBasicAuto(new Pose2d(0.80, 6.60, new Rotation2d(60)), List.of(new Translation2d(0.9, 6.8)), new Pose2d(2.9, 7, new Rotation2d(0))));
-        autoChooser.addOption("shootandrpm", rc.sequenceFactory.getSetRPMandShootCommand(6000, 45));
-        autoChooser.addOption("turnandshootandrpm", rc.sequenceFactory.getTurnSetRPMandShootCommand(-20, 6000, 45));
-
-
+        autoChooser.addOption("blue center", makeBasicAuto(
+            new Pose2d(1.2, 5.55, new Rotation2d(Math.toRadians(0))), 
+            new Pose2d(2.9, 5.55, new Rotation2d(Math.toRadians(0)))));
+        autoChooser.addOption("blue source", makeBasicAuto(
+            new Pose2d(0.8, 4.4, new Rotation2d(Math.toRadians(-60))),
+            List.of(new Translation2d(0.8, 4.2)), new Pose2d(2.9, 4.1, 
+            new Rotation2d(0))));
+        autoChooser.addOption("blue amp", makeBasicAuto(
+            new Pose2d(0.80, 6.60, new Rotation2d(Math.toRadians(60))), 
+            List.of(new Translation2d(0.9, 6.8)), 
+            new Pose2d(2.9, 7, new Rotation2d(Math.toRadians(0)))));
+        autoChooser.addOption("red center", makeBasicAuto(
+            new Pose2d(16.6-1.2, 5.55, new Rotation2d(Math.toRadians(180))), 
+            new Pose2d(16.6-2.9, 5.55, new Rotation2d(Math.toRadians(180)))));
+        autoChooser.addOption("red source", makeBasicAuto(
+            new Pose2d(16.6-0.8, 4.4, new Rotation2d(Math.toRadians(-120))),
+            List.of(new Translation2d(16.6-1.0, 4.4-0.34)), 
+            new Pose2d(16.6-2.9, 4.1, new Rotation2d(Math.toRadians(180)))));
+        autoChooser.addOption("red amp", makeBasicAuto(
+            new Pose2d(16.6-0.80, 6.60, new Rotation2d(Math.toRadians(120))), 
+            List.of(new Translation2d(16.6-1.0, 6.6+0.34)), 
+            new Pose2d(16.6-2.9, 7, new Rotation2d(Math.toRadians(180)))));
+        
         SmartDashboard.putData("Auto Chooser", autoChooser);
 
     }
@@ -186,11 +201,15 @@ public class AutoFactory {
         if(DriverStation.getAlliance().get() == DriverStation.Alliance.Blue){//Checking if blue
             gyroOffset = -start.getRotation().getRadians();
         }
-        final double fgyroOffset = gyroOffset;
+        else{
+            gyroOffset = MathUtil.angleModulus(Math.PI-start.getRotation().getRadians());
+        }
+        final double fgyroOffset = Math.toDegrees(gyroOffset);
 
         return new InstantCommand()
         .andThen(()->rc.chassis.setFieldCentricOffset(fgyroOffset))
-        .andThen(()->rc.chassis.resetOdometry(new Pose2d(start.getX(),start.getY(), rc.navx.getRotation2d())))
+        .andThen(()->rc.chassis.resetOdometry(start))
+        // .andThen(()->rc.chassis.resetOdometry(new Pose2d(start.getX(),start.getY(), rc.navx.getRotation2d())))
         .andThen(rc.sequenceFactory.getSetRPMandShootCommand(6000, 45))
         .andThen(new ParallelDeadlineGroup(
             generateSwerveControllerCommand(start, midpoints, end).withTimeout(5),
@@ -204,264 +223,38 @@ public class AutoFactory {
         return makeBasicAuto(start, List.of(), end);
     }
 
-    public Command getTwoMeterForwardTrajectory(){
-        return 
-        new InstantCommand()
-            .andThen(()->rc.chassis.setFieldCentricOffset(-90))
-            .andThen(()->rc.chassis.resetOdometry(new Pose2d(0,0, rc.navx.getRotation2d())))
-            .andThen(
-                generateSwerveControllerCommand(
-                    TrajectoryGenerator.generateTrajectory(
-                    new Pose2d(0, 0, new Rotation2d(Math.PI/2)),
-                    List.of(),
-                    new Pose2d(2,0, new Rotation2d()),
-                    trajectoryConfig)
-                )
-            )
-            .andThen(
-                generateSwerveControllerCommand(
-                    TrajectoryGenerator.generateTrajectory(
-                    new Pose2d(2, 0, new Rotation2d(Math.PI/2)),
-                    List.of(),
-                    new Pose2d(2,2, new Rotation2d()),
-                    trajectoryConfig)
-                )
-            );
-    }
+
 
     public SendableChooser<Command> getAutoChooser(){
         return autoChooser;
     }
 
+    // Example
+    // public Command getBlueBottomAuto(){
+    //     return new InstantCommand()
+    //     .andThen(rc.sequenceFactory.getSetRPMandShootCommand(6000, 45))
+    //     .andThen(new ParallelDeadlineGroup(
+    //         blueBotStartToBotNotePathCommand().withTimeout(5).andThen(new WaitCommand(1)), 
+    //         rc.sequenceFactory.getIntakeThenAlignCommand()
+    //     ))
+    //     .andThen(rc.sequenceFactory.getTurnSetRPMandShootCommand(Math.toRadians(-25), 8000, 20))
+    //     ;
 
-    public Command getBlueBottomAuto(){
-        return new InstantCommand()
-        .andThen(rc.sequenceFactory.getSetRPMandShootCommand(6000, 45))
-        .andThen(new ParallelDeadlineGroup(
-            blueBotStartToBotNotePathCommand().withTimeout(5).andThen(new WaitCommand(1)), 
-            rc.sequenceFactory.getIntakeThenAlignCommand()
-        ))
-        .andThen(rc.sequenceFactory.getTurnSetRPMandShootCommand(Math.toRadians(-25), 8000, 20))
-        ;
+    // }
 
-    }
-
-    public Command getBlueMidAuto(){
-        return new InstantCommand()
-        .andThen(rc.sequenceFactory.getSetRPMandShootCommand(6000, 45))
-        .andThen(new ParallelDeadlineGroup(
-            blueMidStartToMidNotePathCommand().withTimeout(5).andThen(new WaitCommand(1)), 
-            rc.sequenceFactory.getIntakeThenAlignCommand()
-        ))
-        .andThen(rc.sequenceFactory.getTurnSetRPMandShootCommand(Math.toRadians(0), 8000, 20))
-        ;
-
-    }
-
-    public Command getBlueTopAuto(){
-        return new InstantCommand()
-        .andThen(rc.sequenceFactory.getSetRPMandShootCommand(6000, 45))
-        .andThen(new ParallelDeadlineGroup(
-            blueTopStartToTopNotePathCommand().withTimeout(5).andThen(new WaitCommand(1)), 
-            rc.sequenceFactory.getIntakeThenAlignCommand()
-        ))
-        .andThen(rc.sequenceFactory.getTurnSetRPMandShootCommand(Math.toRadians(27), 8000, 20))
-        ;
-
-    }
-
-    public Command getRedBottomAuto(){
-        return new InstantCommand()
-        .andThen(rc.sequenceFactory.getSetRPMandShootCommand(6000, 45))
-        .andThen(new ParallelDeadlineGroup(
-            blueBotStartToBotNotePathCommand().withTimeout(5).andThen(new WaitCommand(1)), 
-            rc.sequenceFactory.getIntakeThenAlignCommand()
-        ))
-        .andThen(rc.sequenceFactory.getTurnSetRPMandShootCommand(Math.toRadians(25), 8000, 20))
-        ;
-
-    }
-
-    public Command getRedMidAuto(){
-        return new InstantCommand()
-        .andThen(rc.sequenceFactory.getSetRPMandShootCommand(6000, 45))
-        .andThen(new ParallelDeadlineGroup(
-            blueMidStartToMidNotePathCommand().withTimeout(5).andThen(new WaitCommand(1)), 
-            rc.sequenceFactory.getIntakeThenAlignCommand()
-        ))
-        .andThen(rc.sequenceFactory.getTurnSetRPMandShootCommand(Math.toRadians(0), 8000, 20))
-        ;
-
-    }
-
-    public Command getRedTopAuto(){
-        return new InstantCommand()
-        .andThen(rc.sequenceFactory.getSetRPMandShootCommand(6000, 45))
-        .andThen(new ParallelDeadlineGroup(
-            blueTopStartToTopNotePathCommand().withTimeout(5).andThen(new WaitCommand(1)), 
-            rc.sequenceFactory.getIntakeThenAlignCommand()
-        ))
-        .andThen(rc.sequenceFactory.getTurnSetRPMandShootCommand(Math.toRadians(-27), 8000, 20))
-        ;
-
-    }
-
-    /*
-     * Generates a auto sequence managing start position, end position, shooting, and trajectory . 
-     * Boolean isBlue is uneccesary but added for the sake of preventing unwanted behaviors
-     */
-    public Command makeStartToNoteAutoSequence(Pose2d startPose, Pose2d endPose, boolean isBlue){
-        var path =  TrajectoryGenerator.generateTrajectory(
-            startPose,
-            List.of(),
-            endPose,
-            trajectoryConfig);
-
-        var pathCommand = generateSwerveControllerCommand(path);
-
-        double offset = 0;
-
-        if(isBlue){
-            offset = -startPose.getRotation().getRadians();
-        }
-        else{
-            offset = Math.PI - startPose.getRotation().getRadians();
-        }
-
-        offset = Math.toDegrees(MathUtil.angleModulus(offset));
-        final double foffset = offset;
-
-        return new InstantCommand()
-        .andThen(()->rc.chassis.setFieldCentricOffset(foffset))
-
-        .andThen(rc.sequenceFactory.getSetRPMandShootCommand(6000, 45))
-        .andThen(new ParallelDeadlineGroup(
-            pathCommand.withTimeout(path.getTotalTimeSeconds()+1).andThen(new WaitCommand(1)), 
-            rc.sequenceFactory.getIntakeThenAlignCommand()
-        ));
-    }
-
-    /*
-     * Generates a auto sequence managing start position, end position, shooting, and trajectory . 
-     * Boolean isBlue is uneccesary but added for the sake of preventing unwanted behaviors
-     * Overloaded constructor
-     */
-
-    public Command getTwoMeterPathPlanner(){
-        //reset positon
-        var path = AutoBuilder.followPath(pathPlannerPath);
-        var start = pathPlannerPath.getPreviewStartingHolonomicPose();
+    // // obsolete (for now)
+    // public Command getTwoMeterPathPlanner(){
+    //     //reset positon
+    //     var path = AutoBuilder.followPath(pathPlannerPath);
+    //     var start = pathPlannerPath.getPreviewStartingHolonomicPose();
         
-        return new InstantCommand()
-        .andThen( ()-> rc.chassis.setFieldCentricOffset(-90) )
-        .andThen( ()->rc.chassis.resetOdometry(new Pose2d(start.getX(),start.getY(),rc.navx.getRotation2d())) )
-        .andThen(path);
+    //     return new InstantCommand()
+    //     .andThen( ()-> rc.chassis.setFieldCentricOffset(-90) )
+    //     .andThen( ()->rc.chassis.resetOdometry(new Pose2d(start.getX(),start.getY(),rc.navx.getRotation2d())) )
+    //     .andThen(path);
         
-        // return AutoBuilder.followPath(pathPlannerPath);
-    }
-
-    public Command blueBotStartToBotNotePathCommand(){
-        return new InstantCommand()
-            .andThen(()->rc.chassis.setFieldCentricOffset(60))
-            .andThen(()->rc.chassis.resetOdometry(new Pose2d(0.75,4.55, rc.navx.getRotation2d())))
-            .andThen(
-                generateSwerveControllerCommand(
-                    TrajectoryGenerator.generateTrajectory(
-                    new Pose2d(0.75, 4.55, new Rotation2d(Math.toRadians(-60))),
-                    List.of(),
-                    new Pose2d(2.80,4.25, new Rotation2d()),
-                    trajectoryConfig)
-                )
-            );
-    }
-
-    public Command blueBotNoteToBotSharePathCommand(){
-        return new InstantCommand()
-            .andThen(
-                generateSwerveControllerCommand(
-                    TrajectoryGenerator.generateTrajectory(
-                    new Pose2d(2.70, 4.10, new Rotation2d(Math.toRadians(-40))),
-                    List.of(new Translation2d(2.70,1.90)),
-                    new Pose2d(8.30, 0.80, new Rotation2d()),
-                    trajectoryConfig)
-                )
-            );
-    }
-
-    public Command blueMidStartToMidNotePathCommand(){
-        return new InstantCommand()
-            .andThen(()->rc.chassis.setFieldCentricOffset(0))
-            .andThen(()->rc.chassis.resetOdometry(new Pose2d(1.2,5.7, rc.navx.getRotation2d())))
-            .andThen(
-                generateSwerveControllerCommand(
-                    TrajectoryGenerator.generateTrajectory(
-                    new Pose2d(1.2,5.7, new Rotation2d(Math.toRadians(0))),
-                    List.of(),
-                    new Pose2d(2.90,5.55, new Rotation2d()),
-                    trajectoryConfig)
-                )
-            );
-    }
-
-    public Command blueTopStartToTopNotePathCommand(){
-        return new InstantCommand()
-            .andThen(()->rc.chassis.setFieldCentricOffset(-60))
-            .andThen(()->rc.chassis.resetOdometry(new Pose2d(0.75,6.55, rc.navx.getRotation2d())))
-            .andThen(
-                generateSwerveControllerCommand(
-                    TrajectoryGenerator.generateTrajectory(
-                    new Pose2d(0.75, 6.55, new Rotation2d(Math.toRadians(60))),
-                    List.of(),
-                    new Pose2d(2.90,7.00, new Rotation2d()),
-                    trajectoryConfig)
-                )
-            );
-    }
-
-    public Command redBotStartToBotNotePathCommand(){
-        return new InstantCommand()
-            .andThen(()->rc.chassis.setFieldCentricOffset(120))
-            .andThen(()->rc.chassis.resetOdometry(new Pose2d(16-0.75,4.55, rc.navx.getRotation2d())))
-            .andThen(
-                generateSwerveControllerCommand(
-                    TrajectoryGenerator.generateTrajectory(
-                    new Pose2d(16-0.75, 4.55, new Rotation2d(Math.toRadians(-120))),
-                    List.of(),
-                    new Pose2d(16-2.80,4.25, new Rotation2d()),
-                    trajectoryConfig)
-                )
-            );
-    }
-
-    public Command redMidStartToMidNotePathCommand(){
-        return new InstantCommand()
-            .andThen(()->rc.chassis.setFieldCentricOffset(-180))
-            .andThen(()->rc.chassis.resetOdometry(new Pose2d(16-1.2,5.7, rc.navx.getRotation2d())))
-            .andThen(
-                generateSwerveControllerCommand(
-                    TrajectoryGenerator.generateTrajectory(
-                    new Pose2d(16-1.2,5.7, new Rotation2d(Math.toRadians(180))),
-                    List.of(),
-                    new Pose2d(16-2.90,5.55, new Rotation2d()),
-                    trajectoryConfig)
-                )
-            );
-    }
-
-    public Command redTopStartToTopNotePathCommand(){
-        return new InstantCommand()
-            .andThen(()->rc.chassis.setFieldCentricOffset(-120))
-            .andThen(()->rc.chassis.resetOdometry(new Pose2d(16-0.75,6.55, rc.navx.getRotation2d())))
-            .andThen(
-                generateSwerveControllerCommand(
-                    TrajectoryGenerator.generateTrajectory(
-                    new Pose2d(16-0.75, 6.55, new Rotation2d(Math.toRadians(120))),
-                    List.of(),
-                    new Pose2d(16-2.90,7.00, new Rotation2d()),
-                    trajectoryConfig)
-                )
-            );
-    }
+    //     // return AutoBuilder.followPath(pathPlannerPath);
+    // }
 
 
 }
