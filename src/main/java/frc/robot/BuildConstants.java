f69dbfb7461abcab4b5313edabce27f73ecf72a3
fortierkids1 <fortierkids1@gmail.com>
2024-10-24 19:47:22 -0700

    Added dependencies

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Crescendo";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 262;
+  public static final String GIT_SHA = "3e10f5c4ad014340d82d393de1f239736c4bd7f9";
+  public static final String GIT_DATE = "2024-10-24 06:44:09 PDT";
+  public static final String GIT_BRANCH = "origin/advantageKit/Connie";
+  public static final String BUILD_DATE = "2024-10-24 07:36:49 PDT";
+  public static final long BUILD_UNIX_TIME = 1729823809422L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,13 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import com.pathplanner.lib.commands.PathfindingCommand;
 
 // import org.littletonrobotics.junction.LogFileUtil;
@@ -36,13 +43,15 @@ import frc.robot.commands.ClimberSetPosition;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer robotContainer;
 
   public static boolean isCompbot=true;
   DriverStation.Alliance colorAlliance;
+
+  
   
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -51,18 +60,18 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
 
-    // Logger.recordMetadata("GitSHA", GitBuildConstants.GIT_SHA);
+    Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
 
-    // if (isReal()) {
-    //   Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
-    //   Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
-    //   // new PowerDistribution(30, ModuleType.kRev); // Enables power distribution logging
-    // } else {
-    //   setUseTiming(false); // Run as fast as possible
-    //   String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
-    //   Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
-    //   Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
-    // }
+    if (isReal()) {
+      Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+      Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+      // new PowerDistribution(30, ModuleType.kRev); // Enables power distribution logging
+    } else {
+      setUseTiming(false); // Run as fast as possible
+      String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+      Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+      Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+    }
 
     // Logger.disableDeterministicTimestamps(); // See "Deterministic Timestamps" in the "Understanding Data Flow" page
     // Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
@@ -81,7 +90,7 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     robotContainer = new RobotContainer();
 
-    addPeriodic(robotContainer.chassis::updateOdometry, 0.005); //wroky?
+    // addPeriodic(robotContainer.chassis::updateOdometry, 0.005); //wroky?
   }
 
   /**
